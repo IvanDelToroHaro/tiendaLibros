93c0edfedee3f98f194154466d92d0effc457067
IvanDelToroHaro <csi25-idelaro@colegioaltair.net>
2025-11-21 10:23:00 +0100

    Soluciones Propuestas Por Don Danny

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,52 +1,73 @@
-function agregarLibro() {
-	let libro = document.getElementById("seleccionable").value
-	let cantidad = document.getElementById("cantidad").value
-	let saldo = document.getElementById("saldo").innerText
+/**
+* El usuario tendrá un saldo de 100€.
+*Puntos extra si hay una casilla de aceptar terminos y no deja enviar la compra hasta que se marque.
+*El usuario selecciona un libro de un select y escribe cuántos quiere comprar en un input.
+*Al pulsar “Agregar”, se valida que la cantidad sea positiva y se muestra en un <ul> el nombre del libro y la cantidad agregada.
+*Mantener un saldo virtual que disminuya con la compra; no se puede comprar más de lo que el saldo permite
+*/
 
-	if (document.getElementById("saldo").innerHTML <= 0) {
-		alert("No le queda saldo")
-	} else {
-		switch (libro) {
-			case "1":
-				if (cantidad >= 0) {
-					document.getElementById("lista").innerHTML += ("<li> Usted a comprado " + cantidad + " libros  de Don Quijote</li>");
-					document.getElementById("saldo").innerHTML = Number(saldo) - (15 * Number(cantidad))
-				} else {
-					alert("No ha elegido una cantidad valida")
-				}
-				break;
-			case "2":
-				if (cantidad >= 0) {
-					document.getElementById("lista").innerHTML += ("<li> Usted a comprado " + cantidad + " libros de  Geronimo Stilton</li>");
-					document.getElementById("saldo").innerHTML = Number(saldo) - (5 * Number(cantidad))
-				} else {
-					alert("No ha elegido una cantidad valida")
-				}
-				break;
-			case "3":
-				if (cantidad >= 0) {
-					document.getElementById("lista").innerHTML += ("<li> Usted a comprado " + cantidad + " libros de 3 Mosqueteros</li>");
-					document.getElementById("saldo").innerHTML = Number(saldo) - (20 * Number(cantidad))
-				} else {
-					alert("No ha elegido una cantidad valida")
-				}
-				break;
-			case "4":
-				if (cantidad >= 0) {
-					document.getElementById("lista").innerHTML += ("<li> Usted a comprado " + cantidad + " libros de El señor de los anillos</li>");
-					document.getElementById("saldo").innerHTML = Number(saldo) - (25 * Number(cantidad))
-				} else {
-					alert("No ha elegido una cantidad valida")
-				}
-				break;
-			case "5":
-				if (cantidad >= 0) {
-					document.getElementById("lista").innerHTML += ("<li> Usted a comprado " + cantidad + " libros de El hobbit</li>");
-					document.getElementById("saldo").innerHTML = Number(saldo) - (10 * Number(cantidad))
-				} else {
-					alert("No ha elegido una cantidad valida")
-				}
-				break;
-		}
-	}
+var saldoUsuario = 100;
+document.getElementById("saldo").innerText="Saldo:"+saldoUsuario; //escribir cuando carga la pagina para que quede bonito
+
+function agregarLibro (){
+   const libroSeleccionado = document.getElementById("seleccionLibros").value;
+   const cantidad = Number( document.getElementById("cantidad").value );
+   let valorLibro=0;
+   let nombreLibro="";
+   switch(libroSeleccionado){
+       case "1":
+           valorLibro=25;
+           nombreLibro="Libro1";
+           break;
+       case "2":
+           valorLibro=40;
+           nombreLibro="Libro2";
+           break;
+       case "3":
+           valorLibro=15;
+           nombreLibro="Libro3";
+           break;
+       case "0":
+           alert("Por favor elija una opción");
+           break;
+       default:
+           alert("Error, no existe esa opción");        
+   }
+   
+   if (cantidad>0){
+       if (saldoUsuario>=cantidad*valorLibro){
+           //comprar
+           saldoUsuario-=cantidad*valorLibro;
+           escribirLista(nombreLibro, cantidad);
+       }
+       else{
+           alert("Saldo insuficiente")
+       }
+   }
+   else{
+       alert("Cantidad incorrecta")
+   }
+   
+}
+
+function escribirLista(nombre, cantidad){
+   const rutaEscribir = document.getElementById("lista");
+   const rutaSaldo = document.getElementById("saldo");
+   
+   rutaSaldo.innerText="Saldo:"+saldoUsuario;
+   rutaEscribir.innerHTML+="<li>Ha comprado"+ cantidad +" de " + nombre + "</li>"
+   
+   
+}
+
+function confirmar(){
+   const validacion = document.getElementById("validacion").checked; //TRUE o FALSE
+   if (validacion){
+       document.getElementById("boton").disabled=false;
+   }
+   else{
+       document.getElementById("boton").disabled=true;
+
+   }
+   
 }
